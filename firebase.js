24d2743a181ad557fd2b46211c4598d1424beca6
Mehtab Alam <mehtaba728@mail.com>
2021-04-12 17:06:40 +0500

    commented out all the comments

--- firebase.js
+++ firebase.js
@@ -32,51 +32,51 @@
 // }
 
 // TODO APP IN JAVASCRIPT WITH FIREBASE REALTIME DATABASE
-const addTodoButton = document.getElementById("add-todo-button");
-let newTodoInput = document.getElementById("new-todo");
-let listDiv = document.getElementById("list");
+// const addTodoButton = document.getElementById("add-todo-button");
+// let newTodoInput = document.getElementById("new-todo");
+// let listDiv = document.getElementById("list");
 
-function addTodo() {
-  // PUTTING DATA IN FIREBASE DATABASE
+// function addTodo() {
+// PUTTING DATA IN FIREBASE DATABASE
 
-  // GENERATING A RANDOM KEY USING FIREBASE
-  let key = firebase.database().ref().push().key;
+// GENERATING A RANDOM KEY USING FIREBASE
+// let key = firebase.database().ref().push().key;
 
-  let todo = {
-    value: newTodoInput.value,
-    key: key,
-  };
-  firebase.database().ref("todos").child(key).set(todo);
-  // CREATING LI TAG AND ITS TEXT
-  const liElement = document.createElement("li");
-  const liText = document.createTextNode(newTodoInput.value);
+//   let todo = {
+//     value: newTodoInput.value,
+//     key: key,
+//   };
+//   firebase.database().ref("todos").child(key).set(todo);
+//   // CREATING LI TAG AND ITS TEXT
+//   const liElement = document.createElement("li");
+//   const liText = document.createTextNode(newTodoInput.value);
 
-  // CREATING EDIT BUTTON
-  const editBtnEl = document.createElement("button");
-  const editBtnText = document.createTextNode("EDIT");
-  editBtnEl.appendChild(editBtnText);
-  editBtnEl.setAttribute("class", "btn btn-warning");
-  editBtnEl.setAttribute("onclick", "editTodo()");
+//   // CREATING EDIT BUTTON
+//   const editBtnEl = document.createElement("button");
+//   const editBtnText = document.createTextNode("EDIT");
+//   editBtnEl.appendChild(editBtnText);
+//   editBtnEl.setAttribute("class", "btn btn-warning");
+//   editBtnEl.setAttribute("onclick", "editTodo()");
 
-  // CREATING DELETE BUTTON
-  const deleteButtonEl = document.createElement("button");
-  const deleteButtonText = document.createTextNode("DELETE");
-  deleteButtonEl.appendChild(deleteButtonText);
-  deleteButtonEl.setAttribute("class", "btn btn-danger");
-  deleteButtonEl.setAttribute("onclick", "deleteTodo(this)");
+//   // CREATING DELETE BUTTON
+//   const deleteButtonEl = document.createElement("button");
+//   const deleteButtonText = document.createTextNode("DELETE");
+//   deleteButtonEl.appendChild(deleteButtonText);
+//   deleteButtonEl.setAttribute("class", "btn btn-danger");
+//   deleteButtonEl.setAttribute("onclick", "deleteTodo(this)");
 
-  liElement.appendChild(liText);
-  liElement.innerHTML += editBtnEl.outerHTML + deleteButtonEl.outerHTML;
-  listDiv.appendChild(liElement);
-  newTodoInput.value = "";
-}
+//   liElement.appendChild(liText);
+//   liElement.innerHTML += editBtnEl.outerHTML + deleteButtonEl.outerHTML;
+//   listDiv.appendChild(liElement);
+//   newTodoInput.value = "";
+// }
 
-function deleteAll() {
-  listDiv.innerHTML = "";
-}
+// function deleteAll() {
+//   listDiv.innerHTML = "";
+// }
 
-function editTodo() {
-  console.log("edit button clicked");
-}
+// function editTodo() {
+//   console.log("edit button clicked");
+// }
 
-function deleteTodo(receivingThis) {}
+// function deleteTodo(receivingThis) {}
